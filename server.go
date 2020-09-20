49f4405195f328adb82538d12ae3509ead9a84e3
Bjerling Edvin <edvin.bjerling@skola.uppsala.se>
2020-09-20 23:39:12 +0200

    collections

--- server.go
+++ server.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
-	//	"strings"
+	"strings"
 )
+
+// TODO search using a form without javascript
+
+type CollectionLink struct {
+	Name string
+	Path string
+}
+
+type CollectionPageData struct {
+	Links []CollectionLink
+
+	Title          string
+	ImageColumnOne []string
+	ImageColumnTwo []string
+}
 
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
@@ -29,42 +44,124 @@ func serveTemplate(w http.ResponseWriter, r *http.Request) {
 	lp := filepath.Join("templates", "layout.html")
 	fp := filepath.Join("static", filepath.Clean(url))
 
-	// First try to serve from static folder
-	info, err := os.Stat(fp)
-	if err == nil {
-		// Serve static file
-		if !info.IsDir() {
-			print("Okje hittade statisk\n")
-			http.ServeFile(w, r, fp)
-		} else {
-			// If static file does not exist try templates folder
-			tp := filepath.Join("templates", filepath.Clean(url), "index.html")
-			_, err := os.Stat(tp)
-			if err == nil {
-				tmpl, err := template.ParseFiles(lp, tp)
-				if err != nil {
-					serveNotFound(w, r)
-				} else {
-					print("serverar templatead fil\n")
-					tmpl.ExecuteTemplate(w, "layout", nil)
-				}
+	if strings.HasPrefix(url, "/collections/") && !strings.Contains(url, ".") {
+		serveCollectionsPage(w, r)
+	} else {
+		// First try to serve from static folder
+		info, err := os.Stat(fp)
+		if err == nil {
+			// Serve static file
+			if !info.IsDir() {
+				fmt.Println("static file found", fp)
+				http.ServeFile(w, r, fp)
 			} else {
-				if os.IsNotExist(err) {
-					// Try to serve directory contents
-					print("serverar som vanlig fil\n")
-					http.ServeFile(w, r, fp)
+				// If static file does not exist try templates folder
+				tp := filepath.Join("templates", filepath.Clean(url), "index.html")
+				_, err := os.Stat(tp)
+				if err == nil {
+					tmpl, err := template.ParseFiles(lp, tp)
+					if err != nil {
+						serveNotFound(w, r)
+					} else {
+						tmpl.ExecuteTemplate(w, "layout", nil)
+					}
+				} else {
+					if os.IsNotExist(err) {
+						// Try to serve directory contents
+						http.ServeFile(w, r, fp)
+					}
 				}
+			}
+		} else {
+			if os.IsNotExist(err) {
+				print("hittade inte " + fp + "\n")
+				serveNotFound(w, r)
+			} else {
+				serveInternalError(w, r)
+			}
+		}
+	}
+}
 
+func serveCollectionsPage(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Path
+	lp := filepath.Join("templates", "layout.html")
+	if url == "/collections/" {
+		tp := filepath.Join("templates", "collections", "index.html")
+		tmpl, err := template.ParseFiles(lp, tp)
+		if err != nil {
+			serveNotFound(w, r)
+		} else {
+			collections := listImageCollections(w, r)
+			links := []CollectionLink{}
+			for _, name := range collections {
+				links = append(links, CollectionLink{
+					Name: strings.Title(name),
+					Path: name + "/",
+				})
+			}
+			data := CollectionPageData{
+				Links: links,
+			}
+			err := tmpl.ExecuteTemplate(w, "layout", data)
+			if err != nil {
+				fmt.Println(err)
 			}
 		}
 	} else {
-		if os.IsNotExist(err) {
-			print("hittade inte " + fp + "\n")
+		tp := filepath.Join("templates", "collections", "template.html")
+		tmpl, err := template.ParseFiles(lp, tp)
+		if err != nil {
 			serveNotFound(w, r)
 		} else {
-			serveInternalError(w, r)
+			title := strings.Title(strings.ReplaceAll(strings.TrimSuffix(strings.TrimPrefix(url, "/"), "/"), "/", " > "))
+			images := listCollectionImages(w, r)
+			columnOne := images[:len(images)/2]
+			columnTwo := images[len(images)/2:]
+			data := CollectionPageData{
+				Title:          title,
+				ImageColumnOne: columnOne,
+				ImageColumnTwo: columnTwo,
+			}
+			err := tmpl.ExecuteTemplate(w, "layout", data)
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
 	}
+}
+
+func listImageCollections(w http.ResponseWriter, r *http.Request) []string {
+	fp := filepath.Join("static", "collections")
+	folders := []string{}
+	err := filepath.Walk(fp, func(path string, info os.FileInfo, err error) error {
+		name := info.Name()
+		if !strings.Contains(name, ".") && name != "collections" {
+			folders = append(folders, name)
+		}
+		return nil
+	})
+	if err != nil {
+		serveInternalError(w, r)
+	}
+	return folders
+}
+
+func listCollectionImages(w http.ResponseWriter, r *http.Request) []string {
+	url := r.URL.Path
+	fp := filepath.Join("static", url)
+	files := []string{}
+	err := filepath.Walk(fp, func(path string, info os.FileInfo, err error) error {
+		if strings.Contains(info.Name(), ".") {
+			files = append(files, info.Name())
+		}
+		return nil
+	})
+	if err != nil {
+		serveNotFound(w, r)
+	}
+
+	return files
 }
 
 func serveNotFound(w http.ResponseWriter, r *http.Request) {
@@ -77,13 +174,7 @@ func serveInternalError(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, filepath.Join("templates", "error.html"))
 }
 
-func removeElementAt(s []string, i int) []string {
+/*func removeElementAt(s []string, i int) []string {
 	s[i] = s[len(s)-1]
 	return s[:len(s)-1]
-}
-
-/*fs := http.FileServer(http.Dir("public/"))
-http.Handle("/", fs)
-
-http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-})*/
+}*/
